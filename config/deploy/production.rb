07f3902f399a91442aec00d64b81dbaa2e6fb1e1
Chris Beer <cabeer@stanford.edu>
2015-10-26 15:38:21 -0700

    Update is_it_working to monitor the stacks NFS mount

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server 'sul-stacks-prod-a.stanford.edu', user: 'stacks', roles: %w{web db app}
 server 'sul-stacks-prod-b.stanford.edu', user: 'stacks', roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
--- config/initializers/is_it_working.rb
+++ config/initializers/is_it_working.rb
@@ -1,4 +1,5 @@
 Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
+  h.check :directory, path: File.join(Settings.stacks.storage_root, 'bb')
   h.check :url, get: Settings.purl.url
   h.check :url, get: Settings.stacks.djatoka_url + '?rft_id=/&svc_id=info:lanl-repo/svc/ping&url_ver=Z39.88-2004'
 
