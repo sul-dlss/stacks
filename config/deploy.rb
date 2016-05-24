9d18c2364681ca43078b095866ab0274d2018947
Naomi Dushay <ndushay@stanford.edu>
2016-05-23 23:38:43 -0700

    cap deploy prompts for git branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'digital_stacks'
 set :repo_url, 'https://github.com/sul-dlss/digital_stacks_rails.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/opt/app/stacks/stacks'
