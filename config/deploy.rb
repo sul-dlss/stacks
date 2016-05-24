b9bca8596eafa33a8e92d17e5120689cb982b190
Naomi Dushay <ndushay@stanford.edu>
2016-05-23 23:36:40 -0700

    cap deploy logging level now info (was debug)

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :deploy_to, '/opt/app/stacks/stacks'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
