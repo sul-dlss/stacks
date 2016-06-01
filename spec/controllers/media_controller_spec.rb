68003e165befc9b05803b05a3dc1655caadc8edb
Naomi Dushay <ndushay@stanford.edu>
2016-06-01 16:50:48 -0700

    media_controller_spec: remove restricted file specs as they are now in media_auth_request_spec

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -36,34 +36,6 @@ describe MediaController, vcr: { record: :new_episodes } do
         expect(subject.status).to eq 404
       end
     end
-
-    context 'for a restricted file' do
-      before do
-        allow(controller).to receive(:authorize!).and_raise CanCan::AccessDenied
-      end
-
-      context 'with an authenticated user' do
-        it 'fails for unauthorized user' do
-          allow(controller).to receive(:current_user).and_return(User.new)
-          expect(subject.status).to eq 403
-        end
-      end
-
-      context 'with an unauthenticated user' do
-        it 'redirects to the webauth login endpoint for media download' do
-          expect(subject).to redirect_to auth_media_download_url(controller.params.symbolize_keys)
-        end
-        context 'additional params' do
-          subject { get :download, id: 'xf680rd3068', file_name: 'xf680rd3068_1', format: 'mp4', ignored: 'ignored' }
-          it 'ignored when redirecting' do
-            expect(subject).not_to redirect_to(auth_media_download_url(controller.params.symbolize_keys))
-            expect(subject).to redirect_to(auth_media_download_url(controller.send(:allowed_params).symbolize_keys))
-            expect(subject).not_to redirect_to('/media/auth/xf680rd3068/xf680rd3068_1.mp4?ignored=ignored')
-            expect(subject).to redirect_to('/media/auth/xf680rd3068/xf680rd3068_1.mp4')
-          end
-        end
-      end
-    end
   end
 
   describe '#stream' do
@@ -90,34 +62,6 @@ describe MediaController, vcr: { record: :new_episodes } do
         subject
         expect { assigns(:media) }.not_to raise_exception
         expect(assigns(:media)).to be_a StacksMediaStream
-      end
-    end
-
-    context 'for a restricted file' do
-      before do
-        allow(controller).to receive(:authorize!).and_raise CanCan::AccessDenied
-      end
-
-      context 'with an authenticated user' do
-        it 'fails for unauthorized user' do
-          allow(controller).to receive(:current_user).and_return(User.new)
-          expect(subject.status).to eq 403
-        end
-      end
-
-      context 'with an unauthenticated user' do
-        it 'redirects to the webauth login endpoint for media stream' do
-          expect(subject).to redirect_to auth_media_stream_url(controller.params.symbolize_keys)
-        end
-        context 'additional params' do
-          subject { get :stream, id: 'xf680rd3068', file_name: 'xf680rd3068_1.mp4', ignored: 'ignored' }
-          it 'ignored when redirecting' do
-            expect(subject).not_to redirect_to(auth_media_stream_url(controller.params.symbolize_keys))
-            expect(subject).to redirect_to(auth_media_stream_url(controller.send(:allowed_params).symbolize_keys))
-            expect(subject).not_to redirect_to('/media/auth/xf680rd3068/xf680rd3068_1.mp4/stream?ignored=ignored')
-            expect(subject).to redirect_to('/media/auth/xf680rd3068/xf680rd3068_1.mp4/stream')
-          end
-        end
       end
     end
   end
