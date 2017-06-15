f6ef37b2f6fab1ee08c7d9a0ac112094ba24ffe9
Johnathan Martin <john.martin@stanford.edu>
2017-06-15 16:04:30 -0700

    a bit of comment, var name, and settings.yml cleanup
    ability.rb:
    * remove a now extraneous comment about `rule.blank?`
    * add explanation about defining abilities using blocks (with link to cancan wiki entry)
    * a bit of space and comma cleanup
    * more descriptive var names in `can :access` definition
    
    settings.yml:
    * remove obsolete feature flag

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,19 +19,22 @@ class Ability
     # here are :read, :create, :update and :destroy.
     #
     # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # If you pass :all, it will apply to every resource. Otherwise, pass a Ruby
     # class of the resource.
     #
     # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
+    # objects. For example, here the user can only update published articles.
     #   can :update, Article, :published => true
+    #
+    # The block argument takes as a parameter an instance of the object for which
+    # permission is being checked. If the block returns true, the user is granted that
+    # ability, otherwise the user is denied that ability. The block is only evaluated
+    # for instances of objects, *not* for classes. If a class is passed to a `can?` or a
+    # `cannot?` that's defined by a block, that check will *always* grant permission.
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-
-    # Note:  rule.blank?  means it's allowed (e.g. 'no-download' would be a value)
+    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities-with-Blocks
 
     can :download, [StacksFile, StacksImage, StacksMediaStream], &:world_downloadable?
     can :download, [StacksFile, StacksImage, StacksMediaStream] do |f|
@@ -59,17 +62,17 @@ class Ability
     can :read_metadata, StacksImage
 
     can :access, [StacksImage, StacksFile, StacksMediaStream] do |f|
-      val, _rule = f.world_rights
-      next true if val
+      world_rights_defined, _rule = f.world_rights
+      next true if world_rights_defined
 
-      val, _rule = f.stanford_only_rights
-      next true if val && user.stanford?
+      stanford_only_rights_defined, _rule = f.stanford_only_rights
+      next true if stanford_only_rights_defined && user.stanford?
 
-      val, _rule = f.agent_rights(user.id)
-      next true if val && user.app_user?
+      agent_rights_defined, _rule = f.agent_rights(user.id)
+      next true if agent_rights_defined && user.app_user?
 
-      val, _rule = f.location_rights(user.location)
-      next true if val
+      location_rights_defined, _rule = f.location_rights(user.location)
+      next true if location_rights_defined
     end
   end
 end
