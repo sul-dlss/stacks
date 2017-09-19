f893845b2e9c81640985ce597255b78fafc78c25
Justin Coyne <jcoyne@justincoyne.com>
2017-09-19 17:07:18 -0500

    Validate the identifier before doing anything

--- app/models/stacks_identifier.rb
+++ app/models/stacks_identifier.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 # The identifier of a stacks resource
 class StacksIdentifier
   def initialize(options = {})
     if options.is_a? String
-      @druid, @file_name = options.sub(/^druid:/, '').split('%2F', 2)
+      parse_identifer(options)
     elsif options[:id]
-      @druid, @file_name = options[:id].sub(/^druid:/, '').split('%2F', 2)
+      parse_identifer(options[:id])
     elsif options[:druid] && options[:file_name]
-      @druid = options[:druid].sub(/^druid:/, '')
+      self.druid = options[:druid]
       @file_name = options[:file_name]
     end
   end
@@ -21,5 +23,23 @@ class StacksIdentifier
       other.file_name == file_name
   end
 
-  attr_reader :druid, :file_name
+  def valid?
+    return true if druid_parts && file_name
+    false
+  end
+
+  attr_reader :druid, :file_name, :druid_parts
+
+  private
+
+  def druid=(druid)
+    @druid = druid.sub(/^druid:/, '')
+    match = @druid.match(/^([a-z]{2})(\d{3})([a-z]{2})(\d{4})$/i)
+    @druid_parts = match
+  end
+
+  def parse_identifer(id)
+    druid, @file_name = id.split('%2F', 2)
+    self.druid = druid
+  end
 end
--- app/services/path_service.rb
+++ app/services/path_service.rb
@@ -2,13 +2,12 @@
 module PathService
   # @param id [StacksIdentifier]
   def self.for(id)
-    match = id.druid.match(/^([a-z]{2})(\d{3})([a-z]{2})(\d{4})$/i)
-    return unless match && id.file_name
+    return unless id.valid?
     File.join(Settings.stacks.storage_root,
-              match[1],
-              match[2],
-              match[3],
-              match[4],
+              id.druid_parts[1],
+              id.druid_parts[2],
+              id.druid_parts[3],
+              id.druid_parts[4],
               id.file_name)
   end
 end
