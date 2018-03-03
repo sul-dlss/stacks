c11a149b35974743d8869b8490446576ee349a26
Justin Coyne <jcoyne@justincoyne.com>
2018-03-02 18:54:50 -0600

    Differentiate 500 vs 503 responses from the image server
    When the error is a 503, typically there is a load balancer problem
    wheras when it's a 500 typically there is a malformed image that needs
    to be corrected.  Splitting this into two different errors will help us
    categorize these in Honeybadger.

--- lib/errors.rb
+++ lib/errors.rb
@@ -8,4 +8,8 @@ module Stacks
   # RetrieveMetadataError is raised when there is an error retrieving metadata
   # from the image server.
   class RetrieveMetadataError < StandardError; end
+
+  # ImageServerUnavailable is raised when the image server returns
+  # 503 Service Unavailable
+  class ImageServerUnavailable < RetrieveMetadataError; end
 end
