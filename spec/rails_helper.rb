ab9cc076b564917ea33e9c861cc2e769dc5e73b5
Chris Beer <cabeer@stanford.edu>
2018-03-16 10:02:01 -0700

    Address new rubocop warnings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ Coveralls.wear!('rails')
 require 'simplecov'
 SimpleCov.start
 
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
