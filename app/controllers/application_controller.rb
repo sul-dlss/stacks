3742e80dc8187f7f0bfcab5f14104ad8c96a1985
Camille Villa <cvilla100@gmail.com>
2018-04-18 11:06:24 -0700

    Fix Rubocop: Prefer :forbidden over 403

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -87,6 +87,6 @@ class ApplicationController < ActionController::Base
   def rescue_can_can(exception)
     Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
 
-    render file: "#{Rails.root}/public/403.html", status: 403, layout: false
+    render file: "#{Rails.root}/public/403.html", status: :forbidden, layout: false
   end
 end
