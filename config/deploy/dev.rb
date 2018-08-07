28c13f9a9fbbf5519e6f5a5da91273ab6b10a1a4
Jack Reed <phillipjreed@gmail.com>
2018-08-07 08:39:57 -0600

    remove pending decommission env

--- config/deploy/dev.rb
+++
@@ -1,6 +0,0 @@
-server 'sul-stacks-dev.stanford.edu', user: 'stacks', roles: %w{web app}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'development'
-
-set :bundle_without, %w{deployment test}.join(' ')
