53179e0aa2705af7738c366aeac26ecd9d864fa9
Justin Coyne <jcoyne@justincoyne.com>
2023-11-08 14:34:03 -0600

    Send file

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -4,7 +4,7 @@
 # Basic application landing pages
 class StacksController < ApplicationController
   def index
-    render file: "#{Rails.root}/public/index.html", layout: false
-
+    # render file: "#{Rails.root}/public/index.html", layout: false
+    send_file '/public/index.html', type: 'text/html; charset=utf-8'
   end
 end
