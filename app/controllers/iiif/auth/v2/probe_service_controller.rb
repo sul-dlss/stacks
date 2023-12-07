1a58bf853591d6f488a9804851a8b2d86df9fbe9
Peter Mangiafico <peter@mangiafico.org>
2023-12-07 12:22:11 -0800

    add additional specs; remove code duplication of locations

--- app/controllers/iiif/auth/v2/probe_service_controller.rb
+++ app/controllers/iiif/auth/v2/probe_service_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# API to create IIIF Authentication access tokens
 module Iiif
   module Auth
+    # API to create IIIF Authentication access tokens
     module V2
       # Check access for IIIF auth v2
       # https://iiif.io/api/auth/2.0/#probe-service
       class ProbeServiceController < ApplicationController
-        # rubocop:disable Metrics/AbcSize
+        # rubocop:disable Metrics/PerceivedComplexity
         def show
           stacks_uri = params[:id] # this is a fully qualified URI to the resource on the stacks that the user is requesting access to
           druid, file_name = stacks_uri.split('/').last(2) # need the druid (without prefix) and the filename in order to check for access
@@ -20,11 +20,13 @@ module Iiif
             response[:status] = 200
           else
             response[:status] = 401
-            if file.stanford_restricted?
+            if file.stanford_restricted? && !file.embargoed?
               response[:heading] = { en: ["Stanford-affiliated? Login to play"] }
               response[:auth_url] = iiif_auth_api_url
+            elsif file.stanford_restricted? && file.embargoed?
+              response[:heading] = { en: ["Content is both Stanford restricted and embargoed until #{file.embargo_release_date.to_date}"] }
             elsif file.embargoed?
-              response[:heading] = { en: ["Content is embargoed until #{file.embargo_release_date}"] }
+              response[:heading] = { en: ["Content is embargoed until #{file.embargo_release_date.to_date}"] }
             elsif file.restricted_by_location?
               response[:heading] = { en: ["Content is restricted to location #{Settings.user.locations.labels.send(file.location)}"] }
             end
@@ -33,7 +35,7 @@ module Iiif
 
           render json: response
         end
-        # rubocop:enable Metrics/AbcSize
+        # rubocop:enable Metrics/PerceivedComplexity
       end
     end
   end
--- spec/requests/iiif/auth/v2/probe_service_spec.rb
+++ spec/requests/iiif/auth/v2/probe_service_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'IIIF auth v2 probe service' do
     allow(Purl).to receive(:public_json).and_return(public_json)
   end
 
-  context 'when the user has access to the resource' do
+  context 'when the user has access to the resource because it is world accessible' do
     let(:public_json) do
       {
         'structural' => {
@@ -49,7 +49,7 @@ RSpec.describe 'IIIF auth v2 probe service' do
     end
   end
 
-  context 'when the user does not have access to a Stanford only resource' do
+  context 'when a Stanford only resource' do
     let(:public_json) do
       {
         'structural' => {
@@ -60,7 +60,7 @@ RSpec.describe 'IIIF auth v2 probe service' do
                   {
                     'filename' => file_name,
                     'access' => {
-                      'view' => 'world',
+                      'view' => 'stanford',
                       'download' => 'stanford'
                     }
                   }
@@ -74,6 +74,83 @@ RSpec.describe 'IIIF auth v2 probe service' do
 
     before do
       stub_rights_xml(stanford_restricted_rights_xml)
+    end
+
+    context 'when the user is logged in as a Stanford user' do
+      let(:user_webauth_stanford_no_loc) { User.new(webauth_user: true, ldap_groups: %w[stanford:stanford]) }
+      let(:current_user) { user_webauth_stanford_no_loc }
+
+      before do
+        allow_any_instance_of(Iiif::Auth::V2::ProbeServiceController).to receive(:current_user).and_return(current_user)
+        get "/iiif/auth/v2/probe?id=#{stacks_uri}"
+      end
+
+      it 'returns a success response' do
+        expect(response).to have_http_status :ok
+        expect(response.parsed_body).to include({
+                                                  "@context" => "http://iiif.io/api/auth/2/context.json",
+                                                  "type" => "AuthProbeResult2",
+                                                  "status" => 200
+                                                })
+      end
+    end
+
+    context 'when the user is not logged in as a Stanford user' do
+      before do
+        get "/iiif/auth/v2/probe?id=#{stacks_uri}"
+      end
+
+      it 'returns a not authorized response' do
+        expect(response).to have_http_status :ok # NOTE: response status from service is OK, status of the resource is in the response body
+        expect(response.parsed_body).to include({
+                                                  "@context" => "http://iiif.io/api/auth/2/context.json",
+                                                  "type" => "AuthProbeResult2",
+                                                  "status" => 401,
+                                                  "heading" => { "en" => ["Stanford-affiliated? Login to play"] },
+                                                  "auth_url" => "http://www.example.com/auth/iiif",
+                                                  "note" => { "en" => ["Access restricted"] }
+                                                })
+      end
+    end
+  end
+
+  context 'when the user does not have access to a location restricted resource' do
+    let(:public_json) do
+      {
+        'structural' => {
+          'contains' => [
+            {
+              'structural' => {
+                'contains' => [
+                  {
+                    'filename' => file_name,
+                    'access' => {
+                      'view' => 'location-based',
+                      'download' => 'location_based',
+                      'location' => 'spec'
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    end
+    let(:rights_xml) do
+      <<-EOF.strip_heredoc
+      <rightsMetadata>
+          <access type="read">
+            <machine>
+              <location>spec</location>
+            </machine>
+          </access>
+        </rightsMetadata>
+      EOF
+    end
+
+    before do
+      stub_rights_xml(rights_xml)
       get "/iiif/auth/v2/probe?id=#{stacks_uri}"
     end
 
@@ -83,12 +160,127 @@ RSpec.describe 'IIIF auth v2 probe service' do
                                                 "@context" => "http://iiif.io/api/auth/2/context.json",
                                                 "type" => "AuthProbeResult2",
                                                 "status" => 401,
-                                                "heading" => { "en" => ["Stanford-affiliated? Login to play"] },
-                                                "auth_url" => "http://www.example.com/auth/iiif",
+                                                "heading" => {
+                                                  "en" => ["Content is restricted to location Special Collections reading room"]
+                                                },
                                                 "note" => { "en" => ["Access restricted"] }
                                               })
     end
   end
 
-  # TODO: add tests for location and embargo restrictions
+  context 'when the user does not have access to a stanford restricted embargoed resource' do
+    let(:public_json) do
+      {
+        'access' => {
+          'embargo' => {
+            "releaseDate" => Time.parse('2099-05-15').getlocal.as_json
+          }
+        },
+        'structural' => {
+          'contains' => [
+            {
+              'structural' => {
+                'contains' => [
+                  {
+                    'filename' => file_name,
+                    'access' => {
+                      'view' => 'stanford',
+                      'download' => 'stanford'
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    end
+    let(:rights_xml) do
+      <<-EOF.strip_heredoc
+      <rightsMetadata>
+          <access type="read">
+            <machine>
+              <embargoReleaseDate>2099-05-15</embargoReleaseDate>
+              <group>stanford</group>
+            </machine>
+          </access>
+        </rightsMetadata>
+      EOF
+    end
+
+    before do
+      stub_rights_xml(rights_xml)
+      get "/iiif/auth/v2/probe?id=#{stacks_uri}"
+    end
+
+    it 'returns a not authorized response' do
+      expect(response).to have_http_status :ok # NOTE: response status from service is OK, status of the resource is in the response body
+      expect(response.parsed_body).to include({
+                                                "@context" => "http://iiif.io/api/auth/2/context.json",
+                                                "type" => "AuthProbeResult2",
+                                                "status" => 401,
+                                                "heading" => {
+                                                  "en" =>
+                                                    ["Content is both Stanford restricted and embargoed until 2099-05-15"]
+                                                },
+                                                "note" => { "en" => ["Access restricted"] }
+                                              })
+    end
+  end
+
+  context 'when the user does not have access to an embargoed resource' do
+    let(:public_json) do
+      {
+        'access' => {
+          'embargo' => {
+            "releaseDate" => Time.parse('2099-05-15').getlocal.as_json
+          }
+        },
+        'structural' => {
+          'contains' => [
+            {
+              'structural' => {
+                'contains' => [
+                  {
+                    'filename' => file_name,
+                    'access' => {
+                      'view' => 'none',
+                      'download' => 'none'
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    end
+    let(:rights_xml) do
+      <<-EOF.strip_heredoc
+      <rightsMetadata>
+          <access type="read">
+            <machine>
+              <embargoReleaseDate>2099-05-15</embargoReleaseDate>
+            </machine>
+          </access>
+        </rightsMetadata>
+      EOF
+    end
+
+    before do
+      stub_rights_xml(rights_xml)
+      get "/iiif/auth/v2/probe?id=#{stacks_uri}"
+    end
+
+    it 'returns a not authorized response' do
+      expect(response).to have_http_status :ok # NOTE: response status from service is OK, status of the resource is in the response body
+      expect(response.parsed_body).to include({
+                                                "@context" => "http://iiif.io/api/auth/2/context.json",
+                                                "type" => "AuthProbeResult2",
+                                                "status" => 401,
+                                                "heading" => { "en" => ["Content is embargoed until 2099-05-15"] },
+                                                "note" => { "en" => ["Access restricted"] }
+                                              })
+    end
+  end
 end
