9fd06dbe96fb1dfa75492e943618ba30880eb2d7
dnoneill <noneill@stanford.edu>
2025-03-10 15:02:27 -0400

    add wms location for geo login

--- config/routes.rb
+++ config/routes.rb
@@ -83,6 +83,7 @@ Rails.application.routes.draw do
   # IIIF Auth V2
   get '/iiif/auth/v2/token' => 'iiif/auth/v2/token#create'
   get '/iiif/auth/v2/probe' => 'iiif/auth/v2/probe_service#show'
+  get '/proxy/geoserver' => 'restricted_geo_server_proxies#show'
   options '/iiif/auth/v2/probe' => 'iiif/auth/v2/probe_service#options_pre_flight'
 
 end
