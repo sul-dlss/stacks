4709d639142018fb8ceaf01ca04e7b32ba3007bf
Justin Coyne <jcoyne@justincoyne.com>
2025-07-31 08:00:48 -0500

    Allow rubocop to autocorrect RSpec/DescribedClass

--- spec/models/stacks_media_token_spec.rb
+++ spec/models/stacks_media_token_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe StacksMediaToken do
   describe '#create_from_encrypted_string' do
     it 'should build an object with the right fields' do
       encrypted_token_str = subject.to_encrypted_string
-      token_from_encrypted_str = StacksMediaToken.send(:create_from_encrypted_string, encrypted_token_str)
+      token_from_encrypted_str = described_class.send(:create_from_encrypted_string, encrypted_token_str)
 
       expect(token_from_encrypted_str.id).to eq id
       expect(token_from_encrypted_str.file_name).to eq file_name
@@ -40,7 +40,7 @@ RSpec.describe StacksMediaToken do
     end
 
     it 'returns false if the token is too old' do
-      expired_timestamp = (StacksMediaToken.max_token_age + 2.seconds).ago
+      expired_timestamp = (described_class.max_token_age + 2.seconds).ago
       allow(subject).to receive(:timestamp).and_return(expired_timestamp)
       expect(subject.send(:token_valid?, id, file_name, user_ip)).to be false
     end
@@ -50,31 +50,31 @@ RSpec.describe StacksMediaToken do
     let(:enc_token_str) { subject.to_encrypted_string }
 
     it 'returns true for a valid token that matches the specified values' do
-      expect(StacksMediaToken.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be true
+      expect(described_class.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be true
     end
 
     it 'returns false if the signature is invalid' do
       invalid_sig_err = ActiveSupport::MessageVerifier::InvalidSignature
-      allow(StacksMediaToken).to receive(:create_from_encrypted_string).with(enc_token_str).and_raise(invalid_sig_err)
-      expect(StacksMediaToken.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be false
+      allow(described_class).to receive(:create_from_encrypted_string).with(enc_token_str).and_raise(invalid_sig_err)
+      expect(described_class.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be false
     end
 
     it 'returns false if the encrypted message is invalid' do
       invalid_msg_err = ActiveSupport::MessageEncryptor::InvalidMessage
-      allow(StacksMediaToken).to receive(:create_from_encrypted_string).with(enc_token_str).and_raise(invalid_msg_err)
-      expect(StacksMediaToken.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be false
+      allow(described_class).to receive(:create_from_encrypted_string).with(enc_token_str).and_raise(invalid_msg_err)
+      expect(described_class.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be false
     end
 
     it 'returns false on a token that has been tampered with' do
       invalid_token = "#{enc_token_str}invalid"
-      expect(StacksMediaToken.verify_encrypted_token?(invalid_token, id, file_name, user_ip)).to be false
+      expect(described_class.verify_encrypted_token?(invalid_token, id, file_name, user_ip)).to be false
     end
 
     it 'returns false if token_valid? returns false' do
-      mock_token = double(StacksMediaToken)
-      allow(StacksMediaToken).to receive(:create_from_encrypted_string).with(enc_token_str).and_return(mock_token)
+      mock_token = double(described_class)
+      allow(described_class).to receive(:create_from_encrypted_string).with(enc_token_str).and_return(mock_token)
       allow(mock_token).to receive(:token_valid?).with(id, file_name, user_ip).and_return(false)
-      expect(StacksMediaToken.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be false
+      expect(described_class.verify_encrypted_token?(enc_token_str, id, file_name, user_ip)).to be false
     end
   end
 
@@ -85,32 +85,32 @@ RSpec.describe StacksMediaToken do
     let(:valid_ip) { '192.168.1.100' }
 
     it 'can create a valid token without raising an error' do
-      expect { StacksMediaToken.new(valid_id1, valid_filename, valid_ip) }.not_to raise_error
-      expect { StacksMediaToken.new(valid_id2, valid_filename, valid_ip) }.not_to raise_error
+      expect { described_class.new(valid_id1, valid_filename, valid_ip) }.not_to raise_error
+      expect { described_class.new(valid_id2, valid_filename, valid_ip) }.not_to raise_error
     end
 
     it 'raises an error when creating a token with an empty required field' do
-      expect { StacksMediaToken.new('', valid_filename, valid_ip) }
+      expect { described_class.new('', valid_filename, valid_ip) }
         .to raise_error(ActiveModel::StrictValidationFailed, "Id can't be blank")
-      expect { StacksMediaToken.new(valid_id1, '', valid_ip) }
+      expect { described_class.new(valid_id1, '', valid_ip) }
         .to raise_error(ActiveModel::StrictValidationFailed, "File name can't be blank")
-      expect { StacksMediaToken.new(valid_id1, valid_filename, '') }
+      expect { described_class.new(valid_id1, valid_filename, '') }
         .to raise_error(ActiveModel::StrictValidationFailed, "User ip can't be blank")
     end
 
     it 'raises an error when creating a token with a bad id' do
-      expect { StacksMediaToken.new('zab012cd34567', valid_filename, valid_ip) }
+      expect { described_class.new('zab012cd34567', valid_filename, valid_ip) }
         .to raise_error(ActiveModel::StrictValidationFailed, 'Id is invalid')
     end
 
     it 'raises an error when creating a token with a bad IP' do
-      expect { StacksMediaToken.new(valid_id1, valid_filename, '127') }
+      expect { described_class.new(valid_id1, valid_filename, '127') }
         .to raise_error(ActiveModel::StrictValidationFailed, 'User ip is invalid')
-      expect { StacksMediaToken.new(valid_id1, valid_filename, '0') }
+      expect { described_class.new(valid_id1, valid_filename, '0') }
         .to raise_error(ActiveModel::StrictValidationFailed, 'User ip is invalid')
-      expect { StacksMediaToken.new(valid_id1, valid_filename, '0.0.0.0.0') }
+      expect { described_class.new(valid_id1, valid_filename, '0.0.0.0.0') }
         .to raise_error(ActiveModel::StrictValidationFailed, 'User ip is invalid')
-      expect { StacksMediaToken.new(valid_id1, valid_filename, 'localhost') }
+      expect { described_class.new(valid_id1, valid_filename, 'localhost') }
         .to raise_error(ActiveModel::StrictValidationFailed, 'User ip is invalid')
     end
   end
