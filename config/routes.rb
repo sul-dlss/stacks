acd88c08488c66c38982f69d43da6619f6a6cad3
dnoneill <noneill@stanford.edu>
2025-08-06 17:06:37 -0400

    remove legacy auth check route, tests, controllers

--- config/routes.rb
+++ config/routes.rb
@@ -25,16 +25,11 @@ Rails.application.routes.draw do
     end
   end
 
-  if Settings.features.streaming_media
-    # stream file_name must include format extension, eg .../oo000oo0000.mp4/verify_token
-    #  other dots do not need to be URL encoded (see media routing specs)
-    constraints id: druid_regex, file_name: %r{[^/]+\.\w+} do
-      get '/media/:id/:file_name/verify_token' => 'media#verify_token'
-      get '/media/:id/:file_name/auth_check' => 'media#auth_check'
-
-      get '/media/druid::id/:file_name/verify_token' => 'media#verify_token'
-      get '/media/druid::id/:file_name/auth_check' => 'media#auth_check'
-    end
+  # stream file_name must include format extension, eg .../oo000oo0000.mp4/verify_token
+  #  other dots do not need to be URL encoded (see media routing specs)
+  constraints id: druid_regex, file_name: %r{[^/]+\.\w+} do
+    get '/media/:id/:file_name/verify_token' => 'media#verify_token'
+    get '/media/druid::id/:file_name/verify_token' => 'media#verify_token'
   end
 
   root 'stacks#index'
