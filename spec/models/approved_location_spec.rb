c27a2f3506474da619d14483a2dd4feebda1a0e7
Justin Coyne <jcoyne@justincoyne.com>
2025-08-07 11:25:11 -0500

    In ApprovedLocation, locatable is always a user

--- spec/models/approved_location_spec.rb
+++ spec/models/approved_location_spec.rb
@@ -6,19 +6,13 @@ RSpec.describe ApprovedLocation do
   subject { described_class.new(locatable).locations }
 
   context "when the locatable object's IP address does exist in the configuration" do
-    let(:locatable) { double('Locatable', ip_address: 'ip.address1') }
+    let(:locatable) { instance_double(User, ip_address: 'ip.address1') }
 
     it { is_expected.to eq %w[location1 location2] }
   end
 
   context "when the locatable object's IP address does not exist in the configuration" do
-    let(:locatable) { double('Locatable', ip_address: 'not.a.configured.ip') }
-
-    it { is_expected.to be_empty }
-  end
-
-  context 'when the locatable object has no IP address' do
-    let(:locatable) { double('NotReallyLocatable') }
+    let(:locatable) { instance_double(User, ip_address: 'not.a.configured.ip') }
 
     it { is_expected.to be_empty }
   end
