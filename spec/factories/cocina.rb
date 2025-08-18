32430b5bc49cffeff2549b523c5f66f62d8511eb
Justin Coyne <jcoyne@justincoyne.com>
2025-08-18 17:05:35 -0500

    Remove legacy file handling
    Fixes #1493

--- spec/factories/cocina.rb
+++ spec/factories/cocina.rb
@@ -5,33 +5,6 @@ module Factories
     { "externalIdentifier" => id }
   end
 
-  def self.legacy_cocina_with_file(id: "druid:nr349ct7889", file_name: 'image.jp2', access: {},
-                                   file_access: { 'view' => 'world', 'download' => 'world' },
-                                   mime_type: 'image/jp2')
-    cocina(id:).merge(
-      'access' => access,
-      'structural' => {
-        'contains' => [
-          {
-            'structural' => {
-              'contains' => [
-                {
-                  'filename' => file_name,
-                  'hasMessageDigests' => [
-                    { 'type' => 'sha1', 'digest' => 'b1a2922356709cc53b85f1b8027982d23b573f80' },
-                    { 'type' => 'md5', 'digest' => '02f77c96c40ad3c7c843baa9c7b2ff2c' }
-                  ],
-                  'hasMimeType' => mime_type,
-                  'access' => file_access
-                }
-              ]
-            }
-          }
-        ]
-      }
-    )
-  end
-
   def self.cocina_with_file(id: "druid:bb000cr7262", file_name: 'image.jp2', access: {},
                             file_access: { 'view' => 'world', 'download' => 'world' },
                             mime_type: 'image/jp2')
--- spec/requests/iiif_spec.rb
+++ spec/requests/iiif_spec.rb
@@ -55,12 +55,12 @@ RSpec.describe 'IIIF API' do
         context 'outside of the location' do
           context 'when the file is not a thumbnail' do
             let(:public_json) do
-              Factories.legacy_cocina_with_file(file_access: {
-                                                  'view' => 'location-based',
-                                                  'download' => 'location-based',
-                                                  'location' => 'location1'
-                                                },
-                                                mime_type: 'image/jpeg')
+              Factories.cocina_with_file(file_access: {
+                                           'view' => 'location-based',
+                                           'download' => 'location-based',
+                                           'location' => 'location1'
+                                         },
+                                         mime_type: 'image/jpeg')
             end
 
             it 'uses the unauthorized status code for the response' do
@@ -71,11 +71,11 @@ RSpec.describe 'IIIF API' do
 
           context 'when the files is a thumbnail' do
             let(:public_json) do
-              Factories.legacy_cocina_with_file(file_access: {
-                                                  'view' => 'location-based',
-                                                  'download' => 'location-based',
-                                                  'location' => 'location1'
-                                                })
+              Factories.cocina_with_file(file_access: {
+                                           'view' => 'location-based',
+                                           'download' => 'location-based',
+                                           'location' => 'location1'
+                                         })
             end
 
             it 'redirects requests to the degraded info.json' do
@@ -90,8 +90,8 @@ RSpec.describe 'IIIF API' do
         context 'at the location' do
           let(:user) { User.new(ip_address: 'ip.address1') }
           let(:public_json) do
-            Factories.legacy_cocina_with_file(file_access: { 'view' => 'location-based', 'download' => 'location-based',
-                                                             'location' => 'location1' })
+            Factories.cocina_with_file(file_access: { 'view' => 'location-based', 'download' => 'location-based',
+                                                      'location' => 'location1' })
           end
 
           before do
@@ -129,7 +129,7 @@ RSpec.describe 'IIIF API' do
 
       context 'where no one can download' do
         let(:public_json) do
-          Factories.legacy_cocina_with_file(file_access: { 'view' => 'world', 'download' => 'none' })
+          Factories.cocina_with_file(file_access: { 'view' => 'world', 'download' => 'none' })
         end
 
         it 'serves up regular info.json (no degraded)' do
@@ -147,7 +147,7 @@ RSpec.describe 'IIIF API' do
 
       context 'where stanford only no download rights' do
         let(:public_json) do
-          Factories.legacy_cocina_with_file(file_access: { 'view' => 'stanford', 'download' => 'none' })
+          Factories.cocina_with_file(file_access: { 'view' => 'stanford', 'download' => 'none' })
         end
 
         it 'redirects to degraded version' do
@@ -233,226 +233,6 @@ RSpec.describe 'IIIF API' do
 
         it 'loads the image' do
           get '/image/iiif/bb000cr7262%2Fimage/pct:3,3,77,77/full/0/default.jpg'
-
-          expect(response.media_type).to eq 'image/jpeg'
-          expect(response).to have_http_status :ok
-        end
-      end
-    end
-  end
-
-  context 'with legacy file layout' do
-    let(:public_json) do
-      Factories.legacy_cocina_with_file
-    end
-
-    context 'with a bare identifier' do
-      it 'redirects base uri requests to the info.json document' do
-        get '/image/iiif/nr349ct7889/abc'
-
-        expect(response).to redirect_to('/image/iiif/nr349ct7889/abc/info.json')
-        expect(response).to have_http_status :see_other
-      end
-    end
-
-    describe 'metadata requests' do
-      it 'handles JSON requests' do
-        get '/image/iiif/nr349ct7889%2Fimage/info.json', headers: { HTTP_ACCEPT: 'application/json' }
-
-        expect(response.media_type).to eq 'application/json'
-        json = response.parsed_body
-        expect(json['tiles']).to eq [{ 'width' => 256, 'height' => 256, 'scaleFactors' => [1, 2, 4, 8, 16] }]
-        expect(response.headers['Link']).to eq '<http://iiif.io/api/image/2/level2.json>;rel="profile"'
-      end
-
-      it 'handles JSON-LD requests' do
-        get '/image/iiif/nr349ct7889%2Fimage/info.json', headers: { HTTP_ACCEPT: 'application/ld+json' }
-
-        expect(response.media_type).to eq 'application/ld+json'
-        json = JSON.parse(response.body) # rubocop:disable Rails/ResponseParsedBody
-        expect(json['tiles']).to eq [{ 'width' => 256, 'height' => 256, 'scaleFactors' => [1, 2, 4, 8, 16] }]
-      end
-
-      context 'for location-restricted documents' do
-        context 'outside of the location' do
-          context 'when the file is not a thumbnail' do
-            let(:public_json) do
-              Factories.legacy_cocina_with_file(file_access: {
-                                                  'view' => 'location-based',
-                                                  'download' => 'location-based', 'location' => 'location1'
-                                                },
-                                                mime_type: 'image/jpeg')
-            end
-
-            it 'uses the unauthorized status code for the response' do
-              get '/image/iiif/nr349ct7889%2Fimage/info.json'
-              expect(response).to have_http_status :unauthorized
-            end
-          end
-
-          context 'when the files is a thumbnail' do
-            let(:public_json) do
-              Factories.legacy_cocina_with_file(file_access: { 'view' => 'location-based', 'download' => 'location-based',
-                                                               'location' => 'location1' })
-            end
-
-            it 'redirects requests to the degraded info.json' do
-              get '/image/iiif/nr349ct7889%2Fimage/info.json'
-              expect(response).to have_http_status :redirect
-              expect(response).to redirect_to('/image/iiif/degraded/nr349ct7889/image/info.json')
-              expect(response.headers['Cache-Control']).to match(/max-age=0/)
-            end
-          end
-        end
-
-        context 'at the location' do
-          let(:user) { User.new(ip_address: 'ip.address1') }
-          let(:public_json) do
-            Factories.legacy_cocina_with_file(file_access: { 'view' => 'location-based', 'download' => 'location-based',
-                                                             'location' => 'location1' })
-          end
-
-          before do
-            allow_any_instance_of(IiifController).to receive(:current_user).and_return(user)
-          end
-
-          it 'uses the ok status code for the response' do
-            get '/image/iiif/nr349ct7889%2Fimage/info.json'
-            expect(response).to have_http_status :ok
-          end
-        end
-      end
-
-      context 'for stanford-restricted documents' do
-        let(:public_json) do
-          Factories.legacy_cocina_with_file(file_access: { 'view' => 'stanford', 'download' => 'stanford' })
-        end
-
-        it 'redirects requests to the degraded info.json' do
-          get '/image/iiif/nr349ct7889/image/info.json'
-          expect(response).to have_http_status :redirect
-          expect(response).to redirect_to('/image/iiif/degraded/nr349ct7889/image/info.json')
-          expect(response.headers['Cache-Control']).to match(/max-age=0/)
-        end
-
-        context 'when connecting to the degraded url' do
-          it 'serves a degraded info.json description for the original file' do
-            get '/image/iiif/degraded/nr349ct7889/image/info.json'
-
-            expect(response).to have_http_status :ok
-            expect(controller.send(:current_image).stacks_file.id).to eq 'nr349ct7889'
-          end
-        end
-      end
-
-      context 'where no one can download' do
-        let(:public_json) do
-          Factories.legacy_cocina_with_file(file_access: { 'view' => 'world', 'download' => 'none' })
-        end
-
-        it 'serves up regular info.json (no degraded)' do
-          get '/image/iiif/nr349ct7889%2Fimage/info.json'
-          expect(response).to have_http_status :ok
-        end
-
-        it 'replaces the sizes element to reflect the only downloadable (thumbnail) size' do
-          get '/image/iiif/nr349ct7889%2Fimage/info.json'
-          json = response.parsed_body
-
-          expect(json['sizes']).to eq [{ 'width' => 266, 'height' => 400 }]
-        end
-      end
-
-      context 'where stanford only no download rights' do
-        let(:public_json) do
-          Factories.legacy_cocina_with_file(file_access: { 'view' => 'stanford', 'download' => 'none' })
-        end
-
-        it 'redirects to degraded version' do
-          get '/image/iiif/nr349ct7889/image/info.json'
-          expect(response).to have_http_status :redirect
-          expect(response).to redirect_to('/image/iiif/degraded/nr349ct7889/image/info.json')
-        end
-      end
-    end
-
-    describe 'image requests for world readable items' do
-      let(:image_path_component) { image_server_path('nr349ct7889', 'image.jp2') }
-
-      context 'when the request is valid' do
-        before do
-          stub_request(:get, "http://imageserver-prod.stanford.edu/iiif/2/#{image_path_component}/0,640,2552,2552/100,100/0/default.jpg")
-            .to_return(status: 200, body: "")
-        end
-
-        it 'loads the image' do
-          get "/image/iiif/nr349ct7889%2Fimage/0,640,2552,2552/100,100/0/default.jpg"
-
-          expect(response.media_type).to eq 'image/jpeg'
-          expect(response).to have_http_status :ok
-        end
-      end
-
-      context 'when additional params are provided' do
-        before do
-          stub_request(:get, "http://imageserver-prod.stanford.edu/iiif/2/#{image_path_component}/0,640,2552,2552/100,100/0/default.jpg")
-            .to_return(status: 200, body: "")
-        end
-
-        it 'is ignored when instantiating StacksImage' do
-          get "/image/iiif/nr349ct7889%2Fimage/0,640,2552,2552/100,100/0/default.jpg?ignored=ignored&host=host"
-
-          expect(response).to have_http_status :ok
-        end
-      end
-
-      context 'when image is missing' do
-        before do
-          stub_request(:get, "http://imageserver-prod.stanford.edu/iiif/2/#{image_path_component}/0,640,2552,2552/100,100/0/default.jpg")
-            .to_return(status: 404, body: "")
-        end
-
-        it 'returns 404 Not Found' do
-          get "/image/iiif/nr349ct7889%2Fimage/0,640,2552,2552/100,100/0/default.jpg?ignored=ignored&host=host"
-          expect(response).to have_http_status :not_found
-        end
-      end
-
-      context 'when the object has no filesets (e.g. a collection)' do
-        let(:public_json) do
-          { 'structural' => {} }
-        end
-
-        it 'returns 404 Not Found' do
-          stub_request(:get, "http://imageserver-prod.stanford.edu/iiif/2/nr%2F349%2Fct%2F7889%2Fimage/0,640,2552,2552/100,100/0/default.jpg")
-            .to_return(status: 200, body: "", headers: {})
-          get "/image/iiif/nr349ct7889%2Fimage/0,640,2552,2552/100,100/0/default.jpg"
-          expect(response).to have_http_status :not_found
-        end
-      end
-
-      context 'with the download flag set' do
-        before do
-          stub_request(:get, "http://imageserver-prod.stanford.edu/iiif/2/#{image_path_component}/0,640,2552,2552/100,100/0/default.jpg")
-            .to_return(status: 200, body: "")
-        end
-
-        it 'sets the content-disposition header' do
-          get "/image/iiif/nr349ct7889%2Fimage/0,640,2552,2552/100,100/0/default.jpg?download=true"
-
-          expect(response.headers['Content-Disposition']).to start_with 'attachment'
-          expect(response.headers['Content-Disposition']).to include 'filename="image.jpg"'
-        end
-      end
-
-      context 'with a pct region' do
-        before do
-          stub_request(:get, "http://imageserver-prod.stanford.edu/iiif/2/#{image_path_component}/pct:3.0,3.0,77.0,77.0/full/0/default.jpg")
-            .to_return(status: 200, body: "")
-        end
-
-        it 'loads the image' do
-          get '/image/iiif/nr349ct7889%2Fimage/pct:3,3,77,77/full/0/default.jpg'
 
           expect(response.media_type).to eq 'image/jpeg'
           expect(response).to have_http_status :ok
